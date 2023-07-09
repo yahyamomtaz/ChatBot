f7f52d25aeede9bc693eefacb342b32df61d4f09
yahyamomtaz <yahyamomtaz@gmail.com>
2023-07-09 23:10:15 +0200

    maximum token error!!!

---
+++ Chatbot.py
@@ -0,0 +1,34 @@
+import openai
+import os
+
+# Set up your OpenAI API credentials
+openai.api_key = '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'
+
+# Function to read law data from text files
+def read_law_data(file_paths):
+    law_data = []
+    for file_path in file_paths:
+        with open(file_path, 'r') as file:
+            law_content = file.read().strip()
+            law_data.append(law_content)
+    return law_data
+
+# Function to get AI-generated response based on user input and law data
+def get_ai_response(user_input, law_data):
+    prompt = f"User: {user_input}\nData: " + "\n".join(law_data)
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100,
+        temperature=0.7
+    )
+    answer = response.choices[0].text.strip().split('\n')[0]
+    return answer
+
+# Example usage
+file_paths = ['/Users/yaya/Desktop/Text Mining Project/ChatBot/DIVISION OF ASSETS AFTER DIVORCE.txt']
+law_data = read_law_data(file_paths)
+
+user_query = input("Enter your query: ")
+response = get_ai_response(user_query, law_data)
+print(response)
