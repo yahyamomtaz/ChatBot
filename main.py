4d3fd6ad444d02ed2ff53c3c534c513099bd443c
yahyamomtaz <yahyamomtaz@gmail.com>
2023-07-14 13:51:36 +0200

    test3

--- main.py
+++ main.py
@@ -1,117 +1,69 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
+)
 import streamlit as st
+from streamlit_chat import message
+from utilities import *
 
-import sidebar
+st.subheader("Chatbot with Langchain, ChatGPT, Pinecone, and Streamlit")
 
-from ui import (
-    wrap_doc_in_html,
-    is_query_valid,
-    is_file_valid,
-    is_open_ai_key_valid,
-    display_file_read_error,
-)
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
 
-from caching import bootstrap_caching
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
 
-from parsing import read_file
-from chunking import chunk_file
-from embedding import embed_files
-from qa import query_folder
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="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")
 
-EMBEDDING = "openai"
-VECTOR_STORE = "faiss"
-MODEL = "openai"
-
-# For testing
-# EMBEDDING, VECTOR_STORE, MODEL = ["debug"] * 3
-
-st.set_page_config(page_title="KnowledgeGPT", page_icon="📖", layout="wide")
-st.header("📖KnowledgeGPT")
-
-# Enable caching for expensive functions
-bootstrap_caching()
-
-sidebar()
-
-openai_api_key = st.session_state.get("OPENAI_API_KEY")
+if 'buffer_memory' not in st.session_state:
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
 
 
-if not openai_api_key:
-    st.warning(
-        "Enter your OpenAI API key in the sidebar. You can get a key at"
-        " https://platform.openai.com/account/api-keys."
-    )
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context, 
+and if the answer is not contained within the text below, say 'I don't know'""")
 
 
-uploaded_file = st.file_uploader(
-    "Upload a pdf, docx, or txt file",
-    type=["pdf", "docx", "txt"],
-    help="Scanned documents are not supported yet!",
-)
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
-if not uploaded_file:
-    st.stop()
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
 
-try:
-    file = read_file(uploaded_file)
-except Exception as e:
-    display_file_read_error(e)
-
-chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
-
-if not is_file_valid(file):
-    st.stop()
-
-if not is_open_ai_key_valid(openai_api_key):
-    st.stop()
+conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
 
 
-with st.spinner("Indexing document... This may take a while⏳"):
-    folder_index = embed_files(
-        files=[chunked_file],
-        embedding=EMBEDDING,
-        vector_store=VECTOR_STORE,
-        openai_api_key=openai_api_key,
-    )
-
-with st.form(key="qa_form"):
-    query = st.text_area("Ask a question about the document")
-    submit = st.form_submit_button("Submit")
 
 
-with st.expander("Advanced Options"):
-    return_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
-    show_full_doc = st.checkbox("Show parsed contents of the document")
+# container for chat history
+response_container = st.container()
+# container for text box
+textcontainer = st.container()
 
 
-if show_full_doc:
-    with st.expander("Document"):
-        # Hack to get around st.markdown rendering LaTeX
-        st.markdown(f"<p>{wrap_doc_in_html(file.docs)}</p>", unsafe_allow_html=True)
+with textcontainer:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        with st.spinner("typing..."):
+            conversation_string = get_conversation_string()
+            # st.code(conversation_string)
+            refined_query = query_refiner(conversation_string, query)
+            st.subheader("Refined Query:")
+            st.write(refined_query)
+            context = find_match(refined_query)
+            # print(context)  
+            response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response) 
+with response_container:
+    if st.session_state['responses']:
 
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i],key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
 
-if submit:
-    if not is_query_valid(query):
-        st.stop()
-
-    # Output Columns
-    answer_col, sources_col = st.columns(2)
-
-    result = query_folder(
-        folder_index=folder_index,
-        query=query,
-        return_all=return_all_chunks,
-        model=MODEL,
-        openai_api_key=openai_api_key,
-        temperature=0,
-    )
-
-    with answer_col:
-        st.markdown("#### Answer")
-        st.markdown(result.answer)
-
-    with sources_col:
-        st.markdown("#### Sources")
-        for source in result.sources:
-            st.markdown(source.page_content)
-            st.markdown(source.metadata["source"])
-            st.markdown("---")
+          
